476a807d58a6a853e78424ddd35efd23ad4c20a0
fsilvino <fsilvino@users.noreply.github.com>
2018-05-02 19:52:33 -0300

    Melhoria no banco e geração dos Entity Beans

---
+++ Changebook/Changebook-ejb/src/java/ejb/Mensagem.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Flávio
+ */
+@Entity
+@Table(name = "MENSAGEM")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Mensagem.findAll", query = "SELECT m FROM Mensagem m")
+    , @NamedQuery(name = "Mensagem.findById", query = "SELECT m FROM Mensagem m WHERE m.id = :id")
+    , @NamedQuery(name = "Mensagem.findByTexto", query = "SELECT m FROM Mensagem m WHERE m.texto = :texto")})
+public class Mensagem implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 3000)
+    @Column(name = "TEXTO")
+    private String texto;
+    @JoinColumn(name = "IDLIVRO", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Livro livro;
+    @JoinColumn(name = "IDUSUARIO_DESTINATARIO", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Usuario usuarioDestinatario;
+    @JoinColumn(name = "IDUSUARIO_REMETENTE", referencedColumnName = "ID")
+    @ManyToOne(optional = false)
+    private Usuario usuarioRemetente;
+
+    public Mensagem() {
+    }
+
+    public Mensagem(Integer id) {
+        this.id = id;
+    }
+
+    public Mensagem(Integer id, String texto) {
+        this.id = id;
+        this.texto = texto;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Usuario getUsuarioDestinatario() {
+        return usuarioDestinatario;
+    }
+
+    public void setUsuarioDestinatario(Usuario usuarioDestinatario) {
+        this.usuarioDestinatario = usuarioDestinatario;
+    }
+
+    public Usuario getUsuarioRemetente() {
+        return usuarioRemetente;
+    }
+
+    public void setUsuarioRemetente(Usuario usuarioRemetente) {
+        this.usuarioRemetente = usuarioRemetente;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Mensagem)) {
+            return false;
+        }
+        Mensagem other = (Mensagem) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ejb.Mensagem[ id=" + id + " ]";
+    }
+    
+}
